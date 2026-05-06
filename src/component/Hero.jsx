b7e46478b0f17b105f0ada88dcc5f043e116f00a
Sheikh-Rasel2001 <mhrasel191132@gmail.com>
2026-05-06 14:07:23 +0600

    hero section design

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,46 +1,86 @@
-import { FaMosque } from "react-icons/fa";
-import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import { Link } from "react-router";
+import { FaAngleDoubleRight, FaMosque, FaRegHeart } from "react-icons/fa";
+import { LuUsers } from "react-icons/lu";
+
+const images = [
+    "https://i.ibb.co.com/ksxkRKVX/blood-donation.webp",
+    "https://i.ibb.co.com/35n36jPT/talimi-boithok.jpg",
+    "https://i.ibb.co.com/sdsF7rYB/quran-distribution.jpg",
+    "https://i.ibb.co.com/Q3Y6NHYP/free-medical-camp.jpg",
+    "https://i.ibb.co.com/5WsG3WZJ/tree.jpg"
+];
 
 const Hero = () => {
+    const [index, setIndex] = useState(0);
+
+    // auto change image
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % images.length);
+        }, 4000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="w-full min-h-screen bg-linear-to-r from-[#1a4d3c] to-[#4a7c6c] relative overflow-hidden">
+        <div className="relative w-full h-screen overflow-hidden">
 
-            <div
-                className="absolute inset-0 opacity-10"
-                style={{
-                    backgroundImage: `radial-gradient(circle, white 1px, transparent 1px), radial-gradient(circle, white 1px, transparent 1px)`,
-                    backgroundSize: '20px 20px',
-                    backgroundPosition: '0 0, 10px 10px'
-                }}
-            ></div>
-            <div className="absolute top-20 left-10 w-20 h-20 border border-white/10 rounded-full"></div>
-            <div className="absolute bottom-32 right-20 w-32 h-32 border border-white/10 rounded-full"></div>
-            <div className="absolute top-40 right-40 w-16 h-16 border border-white/10 rounded-full"></div>
-            <div className="max-w-7xl mx-auto p-20">
-                <div className="flex justify-center items-center flex-col max-w-4xl mx-auto">
+            {/* Background Images */}
+            <AnimatePresence>
+                <motion.img
+                    key={index}
+                    src={images[index]}
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1.2 }}
+                    className="absolute inset-0 w-full h-full object-cover"
+                />
+            </AnimatePresence>
+
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black/60"></div>
+
+            {/* Content */}
+            <div className="absolute left-0 ">
+                <div className=" max-w-7xl mx-auto p-10 lg:p-20 flex justify-center items-center flex-col text-center">
                     <motion.button 
-                    initial={{opacity: 0, y: 20}}
-                    animate= {{opacity: 1, y: 0}}
-                    transition={{duration: 0.6}}
-                    className="bg-white/10 backdrop-blur-sm px-6 py-3 border border-gray-400 rounded-2xl text-gray-300 flex items-center gap-2"><FaMosque className="text-[#f59e0b] text-xl" /> بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ</motion.button>
-
-                    <motion.h1 
                     initial={{opacity: 0, y: 30}}
                     animate={{opacity: 1, y: 0}}
-                    transition={{duration: 0.8, delay: 0.2}}
-                    className="text-2xl lg:text-4xl text-white text-shadow-emerald-400 font-normal lg:font-bold text-center mt-6 bangla">আদ-দাওয়াহ ইসলামী পাঠাগার এন্ড দাওয়াহ সেন্টার</motion.h1>
-                    <motion.p 
-                    initial={{opacity: 0, y: 30}}
-                    animate= {{opacity: 1, y: 0}}
-                    transition={{duration: 0.8, delay: 0.4}}
-                    className="text-gray-300 font-semibold bangla"> শাখাহাতী, গোবিন্দগঞ্জ,  গাইবান্ধা</motion.p>
+                    transition={{duration: 0.8}}
+                    className="flex items-center gap-2 text-lg font-semibold text-white px-6 p-3 bg-white/10 backdrop-blur-sm border border-white/20 rounded-xl mb-6"><FaMosque className="text-orange-400"/> بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ</motion.button>
+                    <motion.h1
+                        initial={{ opacity: 0, y: 30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2}}
+                        className="text-2xl lg:text-5xl text-white font-bold bangla">আদ-দাওয়াহ ইসলামী পাঠাগার এন্ড দাওয়াহ সেন্টার</motion.h1>
                     <motion.p
+                        initial={{ opacity: 0, y: 30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.3 }}
+                        className="text-gray-200 text-xs lg:text-lg font-semibold mt-2">শাখাহাতী, গোবিন্দগঞ্জ,  গাইবান্ধা</motion.p>
+                    <motion.p
+                        initial={{ opacity: 0, y: 30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.4 }}
+                        className="max-w-2xl mt-6 text-sm lg:text-xl bangla text-center text-white/60">জ্ঞান, দাওয়াহ ও মানবসেবায় নিবেদিত একটি বিশ্বস্ত প্রতিষ্ঠান।
+                        ইসলামের আলো ছড়িয়ে দিতে এবং মানবকল্যাণে কাজ করাই আমাদের অঙ্গীকার।</motion.p>
+                    <motion.div 
                     initial={{opacity: 0, y: 30}}
                     animate={{opacity: 1, y: 0}}
-                    transition={{duration: 0.8, delay: 0.6}} 
-                    className="max-w-2xl mt-6 text-xl bangla text-center text-white/60">জ্ঞান, দাওয়াহ ও মানবসেবায় নিবেদিত একটি বিশ্বস্ত প্রতিষ্ঠান।
-                        ইসলামের আলো ছড়িয়ে দিতে এবং মানবকল্যাণে কাজ করাই আমাদের অঙ্গীকার।</motion.p>
+                    transition={{duration: 0.8, delay: 0.5}}
+                    className="mt-6 flex flex-col lg:flex-row items-center gap-6">
+                        <Link className="group flex items-center gap-2 px-6 py-4 bg-linear-to-r from-amber-500 to-orange-500 text-white font-semibold rounded-xl text-lg hover:from-amber-600 hover:to-orange-600 transition-all transform shadow-lg hover:shadow-xl hover:-translate-y-1 ">
+                            <FaRegHeart /> Donate Now <FaAngleDoubleRight className="text-xl group-hover:translate-x-1" />
+                        </Link>
+                        <Link className="group flex items-center gap-2 bg-white/10 backdrop-blur-sm px-6 py-4 rounded-xl text-lg text-white font-semibold transform transition-all border border-white/30 hover:bg-white/20 hover:-translate-y-1">
+                            <LuUsers /> Join Us <FaAngleDoubleRight className="text-xl group-hover:translate-x-1" />
+                        </Link>
+                    </motion.div>
                 </div>
+
             </div>
         </div>
     );
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,68 +1,121 @@
 import { useEffect, useState } from "react";
-import logoImg from '../assets/ad-dawah.jpeg';
+import logoImg from '../assets/ad-dawah.png';
 import { Link, NavLink } from "react-router";
-import { FaAngleDown, FaAngleRight, FaRegHeart } from "react-icons/fa";
+import { FaAngleDown, FaAngleRight, FaRegHeart, FaRegUser } from "react-icons/fa";
+import { DiGhostSmall } from "react-icons/di";
+import { IoClose } from "react-icons/io5";
 
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
     const [open, setOpen] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(window.scrollY > 50)
+            setIsScrolled(window.scrollY > 20)
         }
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll)
     }, [])
 
+    const closeAll = () => {
+        setShowMenu(false)
+        setOpen(false)
+    }
+
     return (
-        <header className={`w-full fixed left-0 top-0 z-50 transition-all duration-300 ${isScrolled ?
+        <header className={`w-full fixed left-0 top-0 z-100 transition-all duration-300 ${isScrolled ?
             'bg-white/20 backdrop-blur-sm shadow-sm py-3' :
             'bg-white shadow-lg py-1'
             }`}>
-            <nav className="max-w-7xl mx-auto py-3 px-4 flex justify-between items-center">
+            <nav className="max-w-7xl mx-auto py-4 px-4 flex justify-between items-center">
                 {/* logo */}
                 <div>
                     <Link to={'/'} className={`flex items-center gap-2`}>
-                        <img src={logoImg} alt="Ad-Dawaah" className="w-10 h-10" />
+                        <img src={logoImg} alt="Ad-Dawaah" className="lg:w-10 lg:h-10 w-8 h-8" />
                         <div>
-                            <h2 className="text-xl">আদ-দাওয়াহ ইসলামী</h2>
-                            <p className="text-sm text-gray-700">পাঠাগার</p>
+                            <h2 className="text-sm lg:text-xl">আদ-দাওয়াহ ইসলামী</h2>
+                            <p className="text-xs lg:text-sm text-gray-700">পাঠাগার</p>
                         </div>
                     </Link>
+
                 </div>
                 {/* menu bar */}
-                <div>
-                    <ul className=" flex items-center gap-5">
-                        <NavLink>Home</NavLink>
-                        <NavLink>About</NavLink>
-                        <NavLink>Blog</NavLink>
-                        <NavLink>Projects</NavLink>
+                <div className="">
+                    <ul className="hidden lg:flex items-center gap-5">
+                        <NavLink className={'text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>Home</NavLink>
+                        <NavLink className={'text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>About</NavLink>
+                        <NavLink className={'text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>Blog</NavLink>
+                        <NavLink className={'text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>Projects</NavLink>
                         <div className="relative">
-                            <NavLink onClick={() => setOpen(!open)} className={'flex items-center gap-1 '}>
-                                Home
+                            <NavLink onClick={() => setOpen(!open)} className={'flex items-center gap-1 text-xl font-semibold text-gray-700 hover:text-[#f59e0b]'}>
+                                More
                                 {open ? <FaAngleDown /> : <FaAngleRight />}
                             </NavLink>
-                            <ul className={`absolute top-full left-0 w-40 p-3 flex flex-col gap-2 transform transition-all duration-300 ${open ?
+                            <ul className={`absolute top-14 left-0 w-40 bg-white/60 backdrop-blur-md p-3 rounded flex flex-col gap-2 transform transition-all duration-300 ${open ?
                                 'opacity-100 scale-100 translate-y-0 visible' :
                                 'opacity-0 scale-95 -translate-y-2 invisible'}`}>
-                                <NavLink className={'hover:underline'}>Home 1</NavLink>
-                                <NavLink className={'hover:underline'}>Home 1</NavLink>
-                                <NavLink className={'hover:underline'}>Home 1</NavLink>
-                                <NavLink className={'hover:underline'}>Home 1</NavLink>
+                                <NavLink className={'hover:underline text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>Gallery</NavLink>
+                                <NavLink className={'hover:underline text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>Testimonial</NavLink>
+                                <NavLink className={'hover:underline text-lg font-semibold text-gray-700 hover:text-[#f59e0b]'}>Contact</NavLink>
+                                
                             </ul>
 
                         </div>
 
-
+                        {/* contact and log in info */}
+                        <div className="flex gap-8 items-center">
+                            <button className="flex gap-1 items-center py-2 px-4 bg-[#f59e0b] border border-gray-300 text-lg rounded-lg text-white font-bold"><FaRegHeart /> Donate</button>
+                            <button className="flex gap-1 items-center py-2 px-4 border-2 border-green-700 rounded-lg text-green-400 text-lg font-bold hover:bg-white"><FaRegUser /> Log In</button>
+                        </div>
                     </ul>
                 </div>
-                {/* contact and log in info */}
-                <div>
-                    <button className="flex gap-1 items-center py-2 px-4 bg-[#f59e0b] border border-gray-300 rounded-lg text-white font-bold"><FaRegHeart /> Donate</button>
+
+                {/* mobile button */}
+                <div className="lg:hidden">
+                    <button>
+                        {
+                            showMenu ? <IoClose onClick={() => setShowMenu(false)} size={24} /> : <DiGhostSmall onClick={() => setShowMenu(true)} size={24} />
+                        }
+                    </button>
                 </div>
             </nav>
+            {/* mobile nav */}
+            <div className={`fixed top-[72] right-0 w-full transform lg:hidden transition-transform duration-300 ease-in-out
+                ${isScrolled ?
+                    'bg-white/10 backdrop-blur-sm shadow-sm py-3' :
+                    'bg-white shadow-lg py-2'
+                }
+                ${showMenu ? 'translate-x-0' : 'translate-x-full'}
+                `}>
+                <ul className="flex flex-col items-center gap-5">
+                    <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Home</NavLink>
+                    <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>About</NavLink>
+                    <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Blog</NavLink>
+                    <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Projects</NavLink>
+                    <div className="relative">
+                        <NavLink onClick={() => setOpen(!open)} className={'flex items-center gap-1 text-xl font-semibold text-gray-900'}>
+                            Home
+                            {open ? <FaAngleDown /> : <FaAngleRight />}
+                        </NavLink>
+                        <ul className={`absolute bottom-0 left-full w-40 p-3 flex flex-col gap-2 transform transition-all duration-300 ${open ?
+                            'opacity-100 scale-100 translate-y-0 visible' :
+                            'opacity-0 scale-95 -translate-y-2 invisible'}`}>
+                            <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Home 1</NavLink>
+                            <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Home 1</NavLink>
+                            <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Home 1</NavLink>
+                            <NavLink onClick={closeAll} className={'text-xl font-semibold text-gray-900'}>Home 1</NavLink>
+                        </ul>
+
+                    </div>
+
+                    {/* contact and log in info */}
+                    <div>
+                        <button className="flex gap-1 items-center py-2 px-4 bg-[#f59e0b] border border-gray-300 rounded-lg text-white font-bold"><FaRegHeart /> Donate</button>
+                    </div>
+                </ul>
+            </div>
         </header>
     );
 };
